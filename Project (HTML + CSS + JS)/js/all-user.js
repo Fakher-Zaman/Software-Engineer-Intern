42b6a54979d356ac862eafefef672254ca95d13b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-13 11:31:49 +0500

    Day 32 Code (Part-3) *

---
+++ Project (HTML + CSS + JS)/js/all-user.js
@@ -0,0 +1,49 @@
+const apiEndpoint = 'https://dummyjson.com/users';
+const display = document.querySelector("#display-users");
+const userCountDisplay = document.getElementById('user-count');
+
+const getData = async () => {
+    const res = await fetch(apiEndpoint);
+    const data = await res.json();
+    return data;
+}
+
+const displayUsers = async () => {
+    const payload = await getData();
+
+    userCountDisplay.innerHTML = `Locating ${payload.users.length} User Accounts`;
+
+    let dataDisplay = payload.users.map((user) => {
+        console.log(user);
+        const { image, address, phone, age, firstName, lastName, email, gender, birthDate } = user;
+
+        return `
+        <div class="get-users">
+            <div class="part1-info">
+                <!-- <img src="${image}" alt="img" width="50" height="auto"> -->
+                <p class="avatar">${firstName.charAt(0) + lastName.charAt(0)}</p>
+                <div class="contact-info">
+                    <p><span>${phone}</span><i class="fa-solid fa-square-phone-flip"></i></p>
+                    <p><span>${email}</span><i class="fa-solid fa-square-envelope"></i></p>
+                </div>
+            </div>
+            <div class="part2-info">
+                <p><i class="fa-solid fa-user"></i><span>${firstName} ${lastName}</span></p>
+                <p><i class="fa-solid fa-cake-candles"></i><span>${birthDate}</span></p>
+                <p><i class="fa-solid fa-location-dot"></i><span>${address.address}</span></p>
+            </div>
+            <div class="part3-info">
+                <p><span>Gender:</span>${gender}</p>
+                <p><span>Age:</span>${age}</p>
+            </div>
+            <div class="part4-btns">
+                <button id="edit" type="button">Edit</button>
+                <button id="delete" type="button" style="margin-left: 10px;">Delete</button>
+            </div>
+        </div>
+        `
+    }).join("");
+
+    display.innerHTML = dataDisplay;
+}
+displayUsers();
--- Project (HTML + CSS + JS)/js/home.js
+++ Project (HTML + CSS + JS)/js/home.js
@@ -1,53 +1,43 @@
-const apiEndpoint = 'https://dummyjson.com/users';
-const display = document.querySelector("#display-users");
-const userCountDisplay = document.getElementById('user-count');
+// Get the modal
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('editModal');
+    const editButtons = document.querySelectorAll('.part4-btns button#edit');
+    const closeSpan = document.querySelector('.modal-content .close');
+
+    editButtons.forEach(button => {
+        button.onclick = function () {
+            modal.style.display = 'block';
+        }
+    });
+
+    closeSpan.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    document.getElementById('cancelEdit').onclick = function () {
+        modal.style.display = 'none';
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentLocation = window.location.href;
+    const navLinks = document.querySelectorAll(".topnav a");
+    const toggleNavLinks = document.querySelectorAll(".toggle-topnav a");
+
+    navLinks.forEach(link => {
+        if (link.href === currentLocation) {
+            link.classList.add("active");
+        }
+    });
+
+    toggleNavLinks.forEach(link => {
+        if (link.href === currentLocation) {
+            link.classList.add("active");
+        }
+    });
+});
+
 const displayUserProfile = document.querySelector('.user-data');
-
-const getData = async () => {
-    const res = await fetch(apiEndpoint);
-    const data = await res.json();
-    return data;
-}
-
-const displayUsers = async () => {
-    const payload = await getData();
-
-    userCountDisplay.innerHTML = `Locating ${payload.users.length} User Accounts`;
-
-    let dataDisplay = payload.users.map((user) => {
-        console.log(user);
-        const { image, address, phone, age, firstName, lastName, email, gender, birthDate } = user;
-
-        return `
-        <div class="get-users">
-            <div class="part1-info">
-                <!-- <img src="${image}" alt="img" width="50" height="auto"> -->
-                <p class="avatar">${firstName.charAt(0) + lastName.charAt(0)}</p>
-                <div class="contact-info">
-                    <p><span>${phone}</span><i class="fa-solid fa-square-phone-flip"></i></p>
-                    <p><span>${email}</span><i class="fa-solid fa-square-envelope"></i></p>
-                </div>
-            </div>
-            <div class="part2-info">
-                <p><i class="fa-solid fa-user"></i><span>${firstName} ${lastName}</span></p>
-                <p><i class="fa-solid fa-cake-candles"></i><span>${birthDate}</span></p>
-                <p><i class="fa-solid fa-location-dot"></i><span>${address.address}</span></p>
-            </div>
-            <div class="part3-info">
-                <p><span>Gender:</span>${gender}</p>
-                <p><span>Age:</span>${age}</p>
-            </div>
-            <div class="part4-btns">
-                <button id="edit" type="button">Edit</button>
-                <button id="delete" type="button" style="margin-left: 10px;">Delete</button>
-            </div>
-        </div>
-        `
-    }).join("");
-
-    display.innerHTML = dataDisplay;
-}
-displayUsers();
 
 displayUserProfile.innerHTML = `
     <div class="user-profile">
@@ -109,38 +99,6 @@ crossBar.addEventListener('click', () => {
 
 toggleTopnav.addEventListener('click', () => {
     toggleSection.style.left = '-100%';
-});
-
-// Get the modal
-document.addEventListener('DOMContentLoaded', function () {
-    const modal = document.getElementById('editModal');
-    const editButtons = document.querySelectorAll('.part4-btns button#edit');
-    const closeSpan = document.querySelector('.modal-content .close');
-
-    editButtons.forEach(button => {
-        button.onclick = function () {
-            modal.style.display = 'block';
-        }
-    });
-
-    closeSpan.onclick = function () {
-        modal.style.display = 'none';
-    }
-
-    document.getElementById('cancelEdit').onclick = function () {
-        modal.style.display = 'none';
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const currentLocation = window.location.href;
-    const navLinks = document.querySelectorAll(".topnav a");
-
-    navLinks.forEach(link => {
-        if (link.href === currentLocation) {
-            link.classList.add("active");
-        }
-    });
 });
 
 var lastScrollTop = 0;
