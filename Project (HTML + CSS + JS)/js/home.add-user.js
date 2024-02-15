4b3592fe8ed1087f3a151ae3e0ef8c8ec0f186bd
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-15 10:41:45 +0500

    Day 34 Code (Display Input Values on console)

--- Project (HTML + CSS + JS)/js/home.add-user.js
+++ Project (HTML + CSS + JS)/js/home.add-user.js
@@ -1,10 +1,27 @@
 const addUserButton = document.querySelector('#add-user-btn');
+const userImgInput = document.getElementById('image');
+const userFNameInput = document.getElementById('firstName');
+const userLNameInput = document.getElementById('lastName');
+const userPhoneInput = document.getElementById('phone');
+const userEmailInput = document.getElementById('email');
+const userAddressInput = document.getElementById('address');
+const userBirthDateInput = document.getElementById('birthDate');
+const userAgeInput = document.getElementById('age');
+const userGenderInput = document.getElementById('gender');
 
 addUserButton.addEventListener('click', addUser);
 
 function addUser(e) {
     e.preventDefault();
-
-    const items = e.target;
     console.log("Add User Here");
+
+    console.log(userImgInput.value);
+    console.log(userFNameInput.value);
+    console.log(userLNameInput.value);
+    console.log(userPhoneInput.value);
+    console.log(userEmailInput.value);
+    console.log(userAddressInput.value);
+    console.log(userBirthDateInput.value);
+    console.log(userAgeInput.value);
+    console.log(userGenderInput.value);
 }
