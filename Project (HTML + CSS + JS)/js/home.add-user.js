aaf9ff9e43c971f55ae8bc7ba79e9225ba24622f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-15 16:48:25 +0500

    Day 35 Code (Display the UI of add user in their section)

--- Project (HTML + CSS + JS)/js/home.add-user.js
+++ Project (HTML + CSS + JS)/js/home.add-user.js
@@ -9,7 +9,48 @@ const userBirthDateInput = document.getElementById('birthDate');
 const userAgeInput = document.getElementById('age');
 const userGenderInput = document.getElementById('gender');
 
-// addUserButton.addEventListener('click', addUser);
+const display = document.querySelector("#display-users");
+const userCountDisplay = document.getElementById('user-count');
+
+document.addEventListener('DOMContentLoaded', function () {
+    displayUsers();
+});
+
+function displayUsers() {
+    const localUsers = JSON.parse(localStorage.getItem('users')) || [];
+    userCountDisplay.innerHTML = `Locating ${localUsers.length} User Accounts`;
+
+    let dataDisplay = localUsers.map((user) => {
+        const { image, address, phone, age, firstName, lastName, email, gender, birthDate } = user;
+
+        return `
+        <div class="get-users">
+            <div class="part1-info">
+                <img src="${image}" alt="img" width="50" height="auto">
+                <div class="contact-info">
+                    <p><span>${phone}</span><i class="fa-solid fa-square-phone-flip"></i></p>
+                    <p><span>${email}</span><i class="fa-solid fa-square-envelope"></i></p>
+                </div>
+            </div>
+            <div class="part2-info">
+                <p><i class="fa-solid fa-user"></i><span>${firstName} ${lastName}</span></p>
+                <p><i class="fa-solid fa-cake-candles"></i><span>${birthDate}</span></p>
+                <p><i class="fa-solid fa-location-dot"></i><span>${address.address}</span></p>
+            </div>
+            <div class="part3-info">
+                <p><span>Gender:</span>${gender}</p>
+                <p><span>Age:</span>${age}</p>
+            </div>
+            <div class="part4-btns">
+                <button id="edit" class="edit" type="button">Edit</button>
+                <button id="delete" class="delete" type="button" style="margin-left: 10px;">Delete</button>
+            </div>
+        </div>
+        `;
+    }).join("");
+
+    display.innerHTML = dataDisplay;
+}
 
 function addUser(e) {
     e.preventDefault();
