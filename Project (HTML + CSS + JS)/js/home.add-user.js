1ce58e721fecd1bbd8d3c23c8724edbeb16f39df
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-15 11:41:24 +0500

    Day 34 Code (Post the userData on console by running a addUser Api)

--- Project (HTML + CSS + JS)/js/home.add-user.js
+++ Project (HTML + CSS + JS)/js/home.add-user.js
@@ -9,19 +9,47 @@ const userBirthDateInput = document.getElementById('birthDate');
 const userAgeInput = document.getElementById('age');
 const userGenderInput = document.getElementById('gender');
 
-addUserButton.addEventListener('click', addUser);
+// addUserButton.addEventListener('click', addUser);
 
 function addUser(e) {
     e.preventDefault();
     console.log("Add User Here");
 
-    console.log(userImgInput.value);
-    console.log(userFNameInput.value);
-    console.log(userLNameInput.value);
-    console.log(userPhoneInput.value);
-    console.log(userEmailInput.value);
-    console.log(userAddressInput.value);
-    console.log(userBirthDateInput.value);
-    console.log(userAgeInput.value);
-    console.log(userGenderInput.value);
+    fetch('https://dummyjson.com/users/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            image: userImgInput.value,
+            firstName: userFNameInput.value,
+            lastName: userLNameInput.value,
+            phone: userPhoneInput.value,
+            email: userEmailInput.value,
+            address: userAddressInput.value,
+            birthDate: userBirthDateInput.value,
+            age: userAgeInput.value,
+            gender: userGenderInput.value,
+        })
+    })
+        .then(res => res.json())
+        .then(console.log);
+
+    // console.log(userImgInput.value);
+    // console.log(userFNameInput.value);
+    // console.log(userLNameInput.value);
+    // console.log(userPhoneInput.value);
+    // console.log(userEmailInput.value);
+    // console.log(userAddressInput.value);
+    // console.log(userBirthDateInput.value);
+    // console.log(userAgeInput.value);
+    // console.log(userGenderInput.value);
+
+    // Clear input fields
+    userFNameInput.value = "";
+    userLNameInput.value = "";
+    userPhoneInput.value = "";
+    userEmailInput.value = "";
+    userAddressInput.value = "";
+    userBirthDateInput.value = "";
+    userAgeInput.value = "";
+    userGenderInput.value = "";
 }
