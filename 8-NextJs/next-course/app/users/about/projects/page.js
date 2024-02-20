0e1cf86b27bbd9f71b0f0a7e91ee12efaa6f8b4c
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-20 17:15:26 +0500

    Simple Routing + Nested Routes

---
+++ 8-NextJs/next-course/app/users/about/projects/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>These are all of my Projects</div>
+    )
+}
+
+export default Projects
---
+++ 8-NextJs/next-course/app/users/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div>This is a Users Page</div>
+    )
+}
+
+export default Users
