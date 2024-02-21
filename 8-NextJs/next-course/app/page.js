5904caab256e19d15527334379b3256c4501313b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-21 11:11:24 +0500

    Day 41 Code (Next.js: useRouter Hook)

--- 8-NextJs/next-course/app/page.js
+++ 8-NextJs/next-course/app/page.js
@@ -1,8 +1,33 @@
+'use client'
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Home = () => {
+  const router = useRouter();
+
+  const navigate = (page) => {
+    router.push(page);
+  }
+
   return (
-    <div>Home</div>
+    <section>
+      <h1>useRouter</h1>
+      <Link href="/projects">Projects</Link>
+      <button
+        className='border bg-red-200 ml-5 my-10 px-2 py-4'
+        onClick={() => navigate('about')}
+      >
+        Go To About Page
+      </button>
+      <button
+        className='border bg-slate-200 ml-5 my-10 px-2 py-4'
+        onClick={() => navigate('login')}
+      >
+        Go To Login Page
+      </button>
+    </section>
   )
 }
 
---
+++ 8-NextJs/next-course/app/projects/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>This is a Projects Page</div>
+    )
+}
+
+export default Projects
