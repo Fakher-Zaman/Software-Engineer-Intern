95a03f9c9eed86c23384c6cfd78e8c83f2f6bddc
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-22 15:04:09 +0500

    Day 42 Code (Next.js: Font Optimization)

--- 8-NextJs/next-course/app/page.js
+++ 8-NextJs/next-course/app/page.js
@@ -1,32 +1,16 @@
 'use client'
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: '500' })
 
 const Home = () => {
-  const router = useRouter();
-
-  const navigate = (page) => {
-    router.push(page);
-  }
 
   return (
     <section>
-      <h1>Next Js</h1>
-      <Link href="/projects">Projects</Link>
-      <button
-        className='border bg-red-200 ml-5 my-10 px-2 py-4'
-        onClick={() => navigate('about')}
-      >
-        Go To About Page
-      </button>
-      <button
-        className='border bg-slate-200 ml-5 my-10 px-2 py-4'
-        onClick={() => navigate('login')}
-      >
-        Go To Login Page
-      </button>
+      <h1 className={roboto.className}>Google Fonts</h1>
+      <h2 className={roboto.className}>Hello this is roboto font</h2>
     </section>
   )
 }
