dd6b8ef18037b6241ebe24177c52f0e1d53403bf
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-23 11:00:25 +0500

    Day 43 Code (Next.js: Middlewares - Part 2)

--- 8-NextJs/next-course/middleware.js
+++ 8-NextJs/next-course/middleware.js
@@ -2,11 +2,10 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
     console.log('middleware ran');
-    if (request.nextUrl.pathname !== '/login') {
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    return NextResponse.json({ success: "successfully ran" });
+    // if (request.nextUrl.pathname !== '/login') {
+    //     return NextResponse.redirect(new URL("/login", request.url));
+    // }
+    return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
