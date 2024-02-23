f70c3339bb28a86771fa71402ebc80ca7b1fb5fa
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-02-23 10:55:35 +0500

    Day 43 Code (Next.js: Middlewares - Part 1)

---
+++ 8-NextJs/next-course/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    console.log('middleware ran');
+    if (request.nextUrl.pathname !== '/login') {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.json({ success: "successfully ran" });
+}
+
+export const config = {
+    matcher: ['/userslist/:path*']
+}
