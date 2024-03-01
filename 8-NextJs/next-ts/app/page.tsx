b45c511f525a4f49bf2767be85686dd7e100c3c4
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-01 12:23:49 +0500

    Day 47 Code (Next.js: useState & TypeScript)

--- 8-NextJs/next-ts/app/page.tsx
+++ 8-NextJs/next-ts/app/page.tsx
@@ -1,22 +1,9 @@
-import AdminInfo from "@/components/AdminInfo";
-import UserInfo from "@/components/UserInfo";
+import Counter from "@/components/Counter";
 
 export default function Home() {
   return (
     <section>
-      <UserInfo
-        username="fakher"
-        email="fakher@gmail.com"
-        age={20}
-        location={['Earth', 'USA']}
-      />
-      <AdminInfo
-        username="admin"
-        email="admin@gmail.com"
-        age={20}
-        location={['Earth', 'USA']}
-        admin="yes"
-      />
+      <Counter />
     </section>
   );
 }
