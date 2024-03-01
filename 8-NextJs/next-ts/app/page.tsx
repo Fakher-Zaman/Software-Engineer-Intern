2d5bdbaf92d2e5fae497184e17950629e9b912cb
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-01 16:43:39 +0500

    Day 47 Code (Next.js: ElementWrapper)

--- 8-NextJs/next-ts/app/page.tsx
+++ 8-NextJs/next-ts/app/page.tsx
@@ -1,9 +1,21 @@
-import Form from "@/components/Form";
+'use client'
+
+import ElementWrapper from "@/components/ElementWrapper";
 
 export default function Home() {
   return (
     <section>
-      <Form />
+      <ElementWrapper elementType="div" className="box">
+        <p>This is a dive element with a class name of box</p>
+      </ElementWrapper>
+
+      <ElementWrapper elementType="h1" onClick={() => alert('h1 clicked')}>
+        Header Text
+      </ElementWrapper>
+
+      <ElementWrapper elementType="button" onClick={() => alert('button clicked')}>
+        Click Me
+      </ElementWrapper>
     </section>
   );
 }
