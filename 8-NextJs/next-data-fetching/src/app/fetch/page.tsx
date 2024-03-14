632ea166b018526d3c93974deff2bf8f1852db95
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-14 12:27:30 +0500

    Day 56 Code (Next.js: Improve Fetch data)

--- 8-NextJs/next-data-fetching/src/app/fetch/page.tsx
+++ 8-NextJs/next-data-fetching/src/app/fetch/page.tsx
@@ -21,7 +21,7 @@ const Fetch = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://dummyjson.com/products/1');
+                const response = await fetch('https://dummyjson.com/products/9');
                 const jsonData = await response.json();
                 console.log(jsonData);
                 setData(jsonData);
