49606bcd73bbd5bbab249717fd91d5650631dc69
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-14 15:54:35 +0500

    Day 56 Code (Next.js: React Hooks - useContext Props Drilling Problem Solved)

--- 8-NextJs/react-hooks/app/useContext/page.tsx
+++ 8-NextJs/react-hooks/app/useContext/page.tsx
@@ -1,58 +1,69 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+type UserContextType = {
+    user: string;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
 
 const Context = () => {
-    const [user, setUser] = useState('');
+    const [user, setUser] = useState<string>('Fakher Zaman');
 
     useEffect(() => {
         setUser("Fakher Zaman");
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <>
-            <h1>{`Hello ${user}`}</h1>
-            <Component1 user={user} />
-        </>
+        <UserContext.Provider value={{ user }}>
+            <div className='m-4'>
+                <h1>{`Hello ${user}`}</h1>
+                <Component1 />
+            </div>
+        </UserContext.Provider>
     )
 }
 
-function Component1({ user }: { user: string }) {
+function Component1() {
     return (
-        <>
+        <div>
             <h1>Component 1</h1>
-            <Component2 user={user} />
-        </>
+            <Component2 />
+        </div>
     );
 }
 
-function Component2({ user }: { user: string }) {
+function Component2() {
     return (
         <>
             <h1>Component 2</h1>
-            <Component3 user={user} />
+            <Component3 />
         </>
     );
 }
 
-function Component3({ user }: { user: string }) {
+function Component3() {
     return (
         <>
             <h1>Component 3</h1>
-            <Component4 user={user} />
+            <Component4 />
         </>
     );
 }
-function Component4({ user }: { user: string }) {
+
+function Component4() {
     return (
         <>
             <h1>Component 4</h1>
-            <Component5 user={user} />
+            <Component5 />
         </>
     );
 }
-function Component5({ user }: { user: string }) {
+
+function Component5() {
+    const { user } = useContext(UserContext)!;
+
     return (
         <>
             <h1>Component 5</h1>
