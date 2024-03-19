eba8564c81828228791dd701bf0d4780a9ca9b98
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-19 14:33:35 +0500

    Day 60 Code: Design a Layout

--- project-nextjs/src/app/layout.tsx
+++ project-nextjs/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideNav from "@/components/side-nav";
+import MarginWidthWrapper from "@/components/margin-with-wrapper";
+import Header from "@/components/header";
+import PageWrapper from "@/components/page-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-white${inter.className}`}>
+        <div className="flex">
+          <SideNav />
+          <main className="flex-1">
+            <MarginWidthWrapper>
+              <Header />
+              <PageWrapper>{children}</PageWrapper>
+            </MarginWidthWrapper>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ project-nextjs/src/constants.tsx
@@ -0,0 +1,80 @@
+import { MdOutlineDashboardCustomize, MdOutlineSpaceDashboard, MdOutlineVolumeUp } from "react-icons/md";
+import { LuBarChart, LuPlusSquare, LuBox } from "react-icons/lu";
+import { TbBrowser, TbSettings } from "react-icons/tb";
+import { FiUser } from "react-icons/fi";
+import { BiShow } from "react-icons/bi";
+import { PiGraduationCapBold } from "react-icons/pi";
+
+import { SideNavItem } from './types';
+
+export const SIDENAV_ITEMS: SideNavItem[] = [
+    {
+        title: 'Dashboard',
+        path: '/',
+        icon: <MdOutlineSpaceDashboard />,
+    },
+    {
+        title: 'Contents',
+        path: '/contents',
+        icon: <MdOutlineDashboardCustomize />,
+        submenu: true,
+        subMenuItems: [
+            { title: 'Courses', path: '/contents' },
+            { title: 'Live Classes', path: '/contents/web-design' },
+            { title: 'Mock Test', path: '/contents/graphic-design' },
+            { title: 'Test Series', path: '/contents/graphic-design' },
+            { title: 'Bundles', path: '/contents/graphic-design' },
+            { title: 'Legacy Question Pool', path: '/contents/graphic-design' },
+            { title: 'Question Pool', path: '/contents/graphic-design' },
+            { title: 'Subscriptions', path: '/contents/graphic-design' },
+            { title: 'Newsfeed', path: '/contents/graphic-design' },
+            { title: 'Communities', path: '/contents/graphic-design' },
+            { title: 'Categories', path: '/contents/graphic-design' },
+        ],
+    },
+    {
+        title: 'Overview',
+        path: '/overview',
+        icon: <TbBrowser />,
+    },
+    {
+        title: 'Top Voices',
+        path: '/top-voices',
+        icon: <MdOutlineVolumeUp />,
+    },
+    {
+        title: 'Profile',
+        path: '/profile',
+        icon: <FiUser />,
+    },
+    {
+        title: 'Stats',
+        path: '/stats',
+        icon: <LuBarChart />,
+    },
+    {
+        title: 'Inbox',
+        path: '/inbox',
+        icon: <LuBox />,
+    },
+    {
+        title: 'Join',
+        path: '/join',
+        icon: <LuPlusSquare />,
+    },
+    {
+        title: 'Views',
+        path: '/views',
+        icon: <BiShow />,
+    },
+    {
+        title: 'Certificates',
+        path: '/certificates',
+        icon: <PiGraduationCapBold />,
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: <TbSettings />,
+    },
+]
