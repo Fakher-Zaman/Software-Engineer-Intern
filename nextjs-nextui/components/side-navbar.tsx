179cc1c12d64c364d3ff91c256f7109b617a5db3
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-20 11:07:22 +0500

    Day 63 code: SideNavbar in Next.js + NextUI Task (part-2)

--- nextjs-nextui/components/side-navbar.tsx
+++ nextjs-nextui/components/side-navbar.tsx
@@ -1,8 +1,81 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { SIDENAV_ITEMS } from '@/config/constants';
+import { SideNavItem } from '@/types/sidenav';
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 export const SideNavbar = () => {
     return (
-        <aside className="fixed w-64 h-screen border-r-1 border-gray-700">
-            <h1>Side Navbar Here</h1>
-        </aside>
+        <div className="md:w-60 h-screen flex-1 fixed border-r border-zinc-200 hidden md:flex">
+            <div className="flex flex-col space-y-6 w-full">
+                <div className="flex flex-col space-y-1 md:px-6 ">
+                    {SIDENAV_ITEMS.map((item, idx) => {
+                        return <MenuItem key={idx} item={item} />;
+                    })}
+                </div>
+            </div>
+        </div>
     );
-}
+};
+
+const MenuItem = ({ item }: { item: SideNavItem }) => {
+    const pathname = usePathname();
+    const [subMenuOpen, setSubMenuOpen] = useState(false);
+    const toggleSubMenu = () => {
+        setSubMenuOpen(!subMenuOpen);
+    };
+
+    return (
+        <div className="">
+            {item.submenu ? (
+                <>
+                    <button
+                        onClick={toggleSubMenu}
+                        className={`flex flex-row items-center p-2 rounded-lg hover-bg-zinc-100 w-full justify-between hover:bg-zinc-100 ${pathname.includes(item.path) ? 'bg-zinc-100' : ''
+                            }`}
+                    >
+                        <div className="flex flex-row space-x-4 items-center">
+                            {item.icon}
+                            <span className="font-semibold text-xl  flex">{item.title}</span>
+                        </div>
+
+                        <div className={`${subMenuOpen ? 'rotate-180' : ''} flex`}>
+                            <RiArrowDropDownLine />
+                        </div>
+                    </button>
+
+                    {subMenuOpen && (
+                        <div className="my-2 ml-12 flex flex-col space-y-4">
+                            {item.subMenuItems?.map((subItem, idx) => {
+                                return (
+                                    <Link
+                                        key={idx}
+                                        href={subItem.path}
+                                        className={`${subItem.path === pathname ? 'font-bold' : ''
+                                            }`}
+                                    >
+                                        <span>{subItem.title}</span>
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    )}
+                </>
+            ) : (
+                <Link
+                    href={item.path}
+                    className={`flex flex-row space-x-4 items-center p-2 rounded-lg hover:bg-zinc-100 ${item.path === pathname ? 'bg-zinc-100' : ''
+                        }`}
+                >
+                    {item.icon}
+                    <span className="font-semibold text-xl flex">{item.title}</span>
+                </Link>
+            )}
+        </div>
+    );
+};
