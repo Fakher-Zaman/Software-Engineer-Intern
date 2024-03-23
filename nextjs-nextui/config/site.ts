fa749549c5bd3aa9d7ac69926d9ddd8c7e3698ab
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-23 15:19:43 +0500

    Day 66 Code: SideNavbar in Next.js + NextUI Task (part-28)

--- nextjs-nextui/config/site.ts
+++ nextjs-nextui/config/site.ts
@@ -3,28 +3,6 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
 	name: "Next.js + NextUI",
 	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
-		{
-			label: "Docs",
-			href: "/docs",
-		},
-		{
-			label: "Pricing",
-			href: "/pricing",
-		},
-		{
-			label: "Blog",
-			href: "/blog",
-		},
-		{
-			label: "About",
-			href: "/about",
-		}
-	],
 	navMenuItems: [
 		{
 			label: "Profile",
