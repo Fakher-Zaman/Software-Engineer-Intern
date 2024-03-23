b33562802c44c33615b6c0a991eeb9d40e09a3b7
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-23 15:08:24 +0500

    Day 66 Code: SideNavbar in Next.js + NextUI Task (part-27)

--- nextjs-nextui/components/navbar.tsx
+++ nextjs-nextui/components/navbar.tsx
@@ -24,25 +24,9 @@ export const Navbar = () => {
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
-						<p className="font-bold text-inherit">MedPro</p>
+						<p className="font-semibold text-inherit text-lg">MedPro</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
-				</ul>
 			</NavbarContent>
 
 			<NavbarContent
