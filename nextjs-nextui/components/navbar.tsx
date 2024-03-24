e73515defd5ac5af986d409853b548452ae61b9f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-24 16:07:17 +0500

    Day 67 Code: SideNavbar in Next.js + NextUI Task (part-34)

--- nextjs-nextui/components/navbar.tsx
+++ nextjs-nextui/components/navbar.tsx
@@ -17,12 +17,19 @@ import { siteConfig } from "@/config/site";
 import { SIDENAV_ITEMS } from '@/config/constants';
 import NextLink from "next/link";
 import clsx from "clsx";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export const Navbar = () => {
 	const pathname = usePathname();
+	const [activeAccordionIndex, setActiveAccordionIndex] = useState(-1);
+
+	const handleAccordionToggle = (index) => {
+		setActiveAccordionIndex(index === activeAccordionIndex ? -1 : index);
+	};
 
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky" className="border-b-1">
@@ -51,19 +58,47 @@ export const Navbar = () => {
 			<NavbarMenu>
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{SIDENAV_ITEMS.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									pathname.includes(item.path)
-										? "primary"
-										: "foreground"
-								}
-								href={item.path}
-								size="lg"
-							>
-								{item.title}
-							</Link>
-						</NavbarMenuItem>
+						<div key={`${item}-${index}`}>
+							{item.submenu ? (
+								<Accordion
+									className="p-0"
+									onChange={() => handleAccordionToggle(index)}
+								>
+									<AccordionItem key={index} aria-label={item.title} title={item.title}>
+										<div>
+											{item.subMenuItems.map((subItem, subIndex) => (
+												<Link
+													key={`${subItem}-${subIndex}`}
+													color={
+														pathname.includes(subItem.path)
+															? "primary"
+															: "foreground"
+													}
+													href={subItem.path}
+													size="lg"
+												>
+													{subItem.title}
+												</Link>
+											))}
+										</div>
+									</AccordionItem>
+								</Accordion>
+							) : (
+								<NavbarMenuItem>
+									<Link
+										color={
+											pathname.includes(item.path)
+												? "primary"
+												: "foreground"
+										}
+										href={item.path}
+										size="lg"
+									>
+										{item.title}
+									</Link>
+								</NavbarMenuItem>
+							)}
+						</div>
 					))}
 				</div>
 			</NavbarMenu>
