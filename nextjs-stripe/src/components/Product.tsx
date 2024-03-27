503de02515de8258cb51cada6bc64c28e9bf3935
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-27 10:32:26 +0500

    Day 69 Code: Build Product Component UI

--- nextjs-stripe/src/components/Product.tsx
+++ nextjs-stripe/src/components/Product.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+"use client";
 
-const Product = ({ product }) => {
+interface ProductProps {
+    product: Product;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
     return (
-        <div></div>
+        <div className='border rounded-lg p-4 shadow-md'>
+            <h2 className='text-lg font-semibold'>{product.name}</h2>
+            <p className='text-gray-400'>${product.price.toFixed(2)}</p>
+            <button
+                onClick={() => { }}
+                disabled={false}
+                className='mt-2 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+            >
+                {true ? 'Added to Card' : 'Add to Cart'}
+            </button>
+        </div>
     )
 }
 
