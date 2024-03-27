c820f4bd0ce808811c589e791c3b5c1596e52003
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-27 10:52:38 +0500

    Day 69 Code: Build Cart Component UI

--- nextjs-stripe/src/app/page.tsx
+++ nextjs-stripe/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Cart from "@/components/Cart";
 import Product from "@/components/Product";
 
 const products: Product[] = [
@@ -25,7 +26,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-8">
-        <h1 className="text-3xl">E-Commerce Cart System</h1>
+        <h1 className="text-3xl text-center">E-Commerce Cart System</h1>
         <div className="grid grid-cols-3 gap-4">
           {products.map((product) => {
             return (
@@ -33,6 +34,7 @@ export default function Home() {
             );
           })}
         </div>
+        <Cart />
       </div>
     </main>
   );
--- nextjs-stripe/src/components/Cart.tsx
+++ nextjs-stripe/src/components/Cart.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
+"use client";
 
 const Cart = () => {
     return (
-        <div>Cart</div>
+        <div className='border rounded-lg p-4 shadow-md'>
+            <h2 className='text-lg font-semibold mb-4 text-center'>Your Cart</h2>
+
+            <ul>
+                <li className='flex justify-between items-center mb-2'>
+                    <div>
+                        <p className='font-semibold'>Product 1</p>
+                        <p className='text-gray-400'>$23.09 x 4</p>
+                    </div>
+                    <div className="flex space-x-2">
+                        <button
+                            onClick={() => { }}
+                            className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                        >-</button>
+                        <button
+                            onClick={() => { }}
+                            className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                        >+</button>
+                    </div>
+                </li>
+
+                <li className='flex justify-between items-center mb-2'>
+                    <div>
+                        <p className='font-semibold'>Product 1</p>
+                        <p className='text-gray-400'>$23.09 x 4</p>
+                    </div>
+                    <div className="flex space-x-2">
+                        <button
+                            onClick={() => { }}
+                            className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                        >-</button>
+                        <button
+                            onClick={() => { }}
+                            className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                        >+</button>
+                    </div>
+                </li>
+            </ul>
+
+            <div className="mt-4">
+                <p className="text-lg font-semibold">
+                    Total Amount: $234.223
+                </p>
+            </div>
+
+            <button
+                onClick={() => { }}
+                className="mt-4 px-4 py-2 bg-green-500 text-white hover:bg-green-600 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+            >
+                Buy Now
+            </button>
+        </div>
     )
 }
 
