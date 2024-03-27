9996e52dd3129960cca23b2a737df9cd215fc342
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-27 12:24:55 +0500

    Day 69 Code: Use Context data in Cart component

--- nextjs-stripe/src/components/Cart.tsx
+++ nextjs-stripe/src/components/Cart.tsx
@@ -1,60 +1,82 @@
 "use client";
+import React from "react";
+import { useCart } from "./CartContext";
 
 const Cart = () => {
+    const { cart, incrementQuantity, decrementQuantity, removeFromCart } =
+        useCart();
+
+    const totalAmount = cart.reduce(
+        (total, product) => total + product.price * product.quantity,
+        0
+    );
+
+    const handleDecrement = (productId: number) => {
+        decrementQuantity(productId);
+
+        const product = cart.find((item: Product) => item.id === productId.toString());
+        if (product && product.quantity == 0) {
+            removeFromCart(productId);
+        }
+    };
+
+    const checkout = async () => { };
+
     return (
-        <div className='border rounded-lg p-4 shadow-md'>
-            <h2 className='text-lg font-semibold mb-4 text-center'>Your Cart</h2>
+        <div className="border rounded-lg p-4 shadow-md">
+            <h2 className="text-lg font-semibold mb-4 text-center">Your Cart</h2>
+            {cart.length === 0 ? (
+                <p>Your cart is empty</p>
+            ) : (
+                <ul>
+                    {cart.map((product) => (
+                        <li
+                            key={product.id}
+                            className="flex justify-between items-center mb-2"
+                        >
+                            <div>
+                                <p className="font-semibold">{product.name}</p>
+                                <p className="text-gray-400">
+                                    ${product.price.toFixed(2)} x {product.quantity}
+                                </p>
+                            </div>
+                            <div className="flex space-x-2">
+                                <button
+                                    onClick={() => handleDecrement(Number(product.id))}
+                                    className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                                >
+                                    -
+                                </button>
+                                <button
+                                    onClick={() => incrementQuantity(Number(product.id))}
+                                    className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
 
-            <ul>
-                <li className='flex justify-between items-center mb-2'>
-                    <div>
-                        <p className='font-semibold'>Product 1</p>
-                        <p className='text-gray-400'>$23.09 x 4</p>
+            {cart.length > 0 && (
+                <>
+                    <div className="mt-4">
+                        <p className="text-lg font-semibold">
+                            Total Amount: ${totalAmount.toFixed(2)}
+                        </p>
                     </div>
-                    <div className="flex space-x-2">
-                        <button
-                            onClick={() => { }}
-                            className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                        >-</button>
-                        <button
-                            onClick={() => { }}
-                            className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                        >+</button>
-                    </div>
-                </li>
 
-                <li className='flex justify-between items-center mb-2'>
-                    <div>
-                        <p className='font-semibold'>Product 1</p>
-                        <p className='text-gray-400'>$23.09 x 4</p>
-                    </div>
-                    <div className="flex space-x-2">
-                        <button
-                            onClick={() => { }}
-                            className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                        >-</button>
-                        <button
-                            onClick={() => { }}
-                            className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                        >+</button>
-                    </div>
-                </li>
-            </ul>
-
-            <div className="mt-4">
-                <p className="text-lg font-semibold">
-                    Total Amount: $234.223
-                </p>
-            </div>
-
-            <button
-                onClick={() => { }}
-                className="mt-4 px-4 py-2 bg-green-500 text-white hover:bg-green-600 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-            >
-                Buy Now
-            </button>
+                    <button
+                        onClick={(checkout)}
+                        className="mt-4 px-4 py-2 bg-green-500 text-white hover:bg-green-600 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                    >
+                        Buy Now
+                    </button>
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Cart;
