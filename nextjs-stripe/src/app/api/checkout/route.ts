a8cfff64cd3135c7a543f4766c77e5cadd04be24
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-28 14:13:12 +0500

    Day 69 Code: Add api route in next.js 13 for stripe payment processing

--- nextjs-stripe/src/app/api/checkout/route.ts
+++ nextjs-stripe/src/app/api/checkout/route.ts
@@ -2,9 +2,30 @@ import { NextResponse } from "next/server";
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
+const getActiveProducts = async () => {
+    const checkProducts = await stripe.products.list();
+    const availableProducts = checkProducts.data.filter((product: any) => product.active === true);
+
+    return availableProducts;
+}
+
 export const POST = async (request: any) => {
     const { products } = await request.json();
     const data: Product[] = products;
+
+    let activeProducts = await getActiveProducts();
+
+    try {
+        for (const product of data) {
+            const stripeProduct = activeProducts?.find(
+                (stripeProduct: any) => {
+                    stripeProduct?.name?.toLowerCase() == product?.name?.toLowerCase();
+                }
+            )
+        }
+    } catch (error) {
+
+    }
 
     const prods = await stripe.products.list();
     console.log(prods);
